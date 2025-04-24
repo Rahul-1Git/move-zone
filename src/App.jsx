7c4f1d136aaaff93180e8fe933634aa2831bafbf
Rahul Rawat <99199444+Rahul-1Git@users.noreply.github.com>
2025-04-24 21:25:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css";
+import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import MovieList from "./components/movieList/MovieList";
 const App = () => {
